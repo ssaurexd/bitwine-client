b976092ece5e52458c10784d94acd65a408a9396
Aure <ssaurexd@gmail.com>
2021-12-13 14:14:51 -0600

    fix devmode

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,18 +1,15 @@
-import { settings } from '../config/settings';
-
-
 export const GA_TRACKING_ID = 'G-N1LF5QKCVG'
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = ( url: URL ) => {
 
 	try {
-		if( !settings.devMode ) {
 
-			window.gtag( 'config', GA_TRACKING_ID, {
-				page_path: url
-			})
-		}
+		window.gtag( 'config', GA_TRACKING_ID, {
+			page_path: url
+		})
 	} catch ( error ) {
+		
 		console.log("Error from the trackerPageView => ", error);
 	}
 }
@@ -27,12 +24,9 @@ type GTagEvent = {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent ) => {
 
-	if( !settings.devMode ) {
-
-		window.gtag( 'event', action, {
-			event_category: category,
-			event_label: label,
-			value: value
-		})
-	}
+	window.gtag( 'event', action, {
+		event_category: category,
+		event_label: label,
+		value: value
+	})
 }
